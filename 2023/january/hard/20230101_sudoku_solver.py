09a63e1c285b58f810fa998328140913dbc5480f
Victor Moraes <victordmor@MacBook-Pro-de-Victor.local>
2023-01-04 20:47:40 -0300

    Daily problem of 2023-01-04

--- 2023/january/hard/20230101_sudoku_solver.py
+++ 2023/january/hard/20230101_sudoku_solver.py
@@ -84,30 +84,97 @@ class Solution:
             solved = True
             for rows in board:
                 for cell in rows:
-                    if cell == '.':
+                    if cell == '.' or isinstance(cell, list):
                         solved = False
                         break
                 if not solved:
                     break
             return solved
 
+        def check_stalled_board(board_state):
+            if len(board_state) >= 2:
+                if board[-1] == board[-2]:
+                    return True
+            return False
+
+        def check_hidden_double(board):
+            candidate_frequency = {
+                1:
+                2:
+                3:
+                4:
+                5:
+                6:
+                7:
+                8:
+                9:
+            }
+
+
+
+        def check_swordfish(board):
+            pass
+
         numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        sub_boxes_rows = {
+            0: [0, 1, 2],
+            1: [0, 1, 2],
+            2: [0, 1, 2],
+            3: [3, 4, 5],
+            4: [3, 4, 5],
+            5: [3, 4, 5],
+            6: [6, 7, 8],
+            7: [6, 7, 8],
+            8: [6, 7, 8],
+        }
+        sub_boxes_cols = {
+            0: [0, 1, 2],
+            1: [3, 4, 5],
+            2: [6, 7, 8],
+            3: [0, 1, 2],
+            4: [3, 4, 5],
+            5: [6, 7, 8],
+            6: [0, 1, 2],
+            7: [3, 4, 5],
+            8: [6, 7, 8],
+        }
+        board_state = []
         solved = False
-        ## To be continued tomorrow
+        board_stalled = False
         while not solved:
             sub_boxes = create_sub_box_board(board)
+            if board_stalled:
+                board = check_hidden_double(board)
             for i in range(len(board)):
                 for j in range(len(board[i])):
                     candidates = []
-                    if board[i][j] != '.':
-                        pass
-                    else:
+                    if board[i][j] == '.' or isinstance(board[i][j], list):
+                        sub_box_index = get_specific_sub_box(i, j)
+                        sub_box = sub_boxes[sub_box_index]
                         for n in numbers:
-                            if n not in board[i] and n not in [x[j] for x in board] and n not in sub_boxes[get_specific_sub_box(i, j)]:
+                            if n not in board[i] and n not in [x[j] for x in board] and n not in sub_box:
                                 candidates.append(n)
-                        print(candidates)
+                            if n in candidates:
+                                present_everywhere = True
+                                for row in sub_boxes_rows[sub_box_index]:
+                                    if row != i and n not in board[row]:
+                                        present_everywhere = False
+                                        break
+                                if present_everywhere:
+                                    for col in sub_boxes_cols[sub_box_index]:
+                                        if col != j and n not in [x[col] for x in board]:
+                                            present_everywhere = False
+                                            break
+                                if present_everywhere:
+                                    candidates = [n]
+                                    break
                         if len(candidates) == 1:
                             board[i][j] = candidates[0]
+                        else:
+                            board[i][j] = candidates
+            board_state.append(board)
+            if not board_stalled:
+                board_stalled = check_stalled_board(board_state)
             if check_solve_condition(board):
                 break
         return board
