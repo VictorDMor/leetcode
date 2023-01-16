8e83888ffca80706e3a862b424590dd34fb08d7b
Victor Moraes <victordmor@MacBook-Pro-de-Victor.local>
2023-01-15 21:10:16 -0300

    Adds problems of 20130112 (not time efficient) and problem no 6

---
+++ 2023/january/medium/20230115_zigzag_conversion.py
@@ -0,0 +1,58 @@
+'''
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: 
+(you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+
+Example 1:
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+
+Example 2:
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+Example 3:
+Input: s = "A", numRows = 1
+Output: "A"
+ 
+
+Constraints:
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+
+'''
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        zigzag_matrix = []
+        i = 0
+        j = 0
+        invert = True
+        while True:
+            if i == numRows - 1 or i == 0:
+                invert = not invert
+            try:
+                zigzag_matrix[i].append(s[j])
+            except IndexError:
+                zigzag_matrix.append([s[j]])
+
+            if invert:
+                i -= 1
+            else:
+                i += 1
+
+            j += 1
+            if j == len(s): break
+        return ''.join([letter for row in zigzag_matrix for letter in row])
